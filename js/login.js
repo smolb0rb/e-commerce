a82b0ae8c33ef254f1a72dc538c66c3beb03bbf6
smolb0rb <38226435+smolb0rb@users.noreply.github.com>
2024-08-23 16:17:13 -0300

    Redirect to index if already logged in

--- js/login.js
+++ js/login.js
@@ -1,9 +1,17 @@
+function isLoggedIn() {
+    return localStorage.getItem("isAuthenticated") === "true"
+}
+
+if (isLoggedIn()) { // redirect to index.html if already logged in
+    document.location = 'index.html'
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
 
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
-    
+
         const username = document.getElementById("username").value.trim();
         const password = document.getElementById("password").value.trim(); 
 
