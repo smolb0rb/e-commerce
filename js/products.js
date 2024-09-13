f32b40a0b54407039475c73dd19cc6acd4291d8c
smolb0rb <38226435+smolb0rb@users.noreply.github.com>
2024-09-13 13:05:31 -0300

    Implementacion demostrativa de busqueda y ordenamiento

--- js/products.js
+++ js/products.js
@@ -1,16 +1,18 @@
+let productsCache = [];
+let lastShown = [];
 
-function showProductsList(productsArray) {
-    let htmlContentToAppend = "";
+function showProducts(productsArray) {
+    let newHtmlContent = "";
 
     // Itera sobre cada producto dentro del array 'products'
     for (let i = 0; i < productsArray.length; i++) {
         let product = productsArray[i];
 
-        htmlContentToAppend += `
+        newHtmlContent += `
         <div class="list-group-item list-group-item-action cursor-active" data-id="${product.id}" onclick="selectProduct(${product.id})">  
             <div class="row">
                 <div class="col-lg-3 col-sm-6">
-                    <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                    <img src="${product.image}" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <small class="float-end text-muted"> Vendidos hasta el momento: ${product.soldCount}</small>
@@ -23,31 +25,23 @@ function showProductsList(productsArray) {
             </div>
         </div>
         `
-
     }
 
     // Inserta el contenido generado en el div
-    document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    document.getElementById("product-list-container").innerHTML = newHtmlContent;
 }   
 
-// petición de json
 document.addEventListener("DOMContentLoaded", function (e) {
 
     let catID = localStorage.getItem("catID");
 
     let url = PRODUCTS_URL + catID + EXT_TYPE;
 
+    getJSONData(url).then(function (result) {
+        if (result.status === "ok") {
+            productsCache = result.data.products;
 
-
-    // Realizar la solicitud a la URL 
-    getJSONData(url).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            console.log(resultObj.data);
-
-            // Acceder a la lista de productos dentro del objeto JSON
-            let productsArray = resultObj.data.products;
-            showProductsList(productsArray);
-            console.log(productsArray);
+            showProducts(productsCache);
         }
     });
 
@@ -59,4 +53,51 @@ function selectProduct(productId){
     localStorage.setItem("selectedProductId",productId);
 
     window.location.href = "product-info.html"; 
+}
+
+// Ordenamiento
+
+function sortByName(asc) {
+    return fnSort(productsCache, p => p.name, asc);
+}
+
+function sortByPrice(asc) {
+    return fnSort(productsCache, p => p.cost, asc);
+}
+
+function sortBySold(asc) {
+    return fnSort(productsCache, p => p.soldCount, asc);
+}
+
+function fnSort(array, fn, asc) {
+    return array.toSorted(function (a, b) {
+        a = fn(a); b = fn(b);
+        if (a < b) { return asc ? -1 : 1; }
+        if (a > b) { return asc ? 1 : -1; }
+        return 0;
+    });
+}
+
+// Funcionamiento de barra de busqueda.
+
+let searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("input", function(e) {
+    let text = e.target.value;
+
+    let result = productsCache.filter(product => {
+        if (contains(product.name, text) || contains(product.description, text)) {
+            return true;
+        }
+        return false;
+    });
+
+    showProducts(result);
+});
+
+/**
+ * Retorna true si el string "find" existe dentro de "str" sin importar las mayusculas y miniusculas.
+ */
+function contains(str, find) {
+    return str.toLowerCase().indexOf(find.toLowerCase()) != -1;
 }
